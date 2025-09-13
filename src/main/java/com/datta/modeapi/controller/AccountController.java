a0be321f1fa85d0798958f0f18a54b24b4d288dd
dattatray001 <dattatrynarhe@gmail.com>
2025-09-13 16:44:46 +0530

    verify otp

--- src/main/java/com/datta/modeapi/controller/AccountController.java
+++ src/main/java/com/datta/modeapi/controller/AccountController.java
@@ -2,7 +2,10 @@ package com.datta.modeapi.controller;
 
 
 import com.datta.modeapi.DTO.Otp;
+import com.datta.modeapi.DTO.getOtp;
+import com.datta.modeapi.DTO.otpResponse;
 import com.datta.modeapi.Entity.Account;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,8 +15,24 @@ public class AccountController {
 
     @PostMapping("/create")
     public ResponseEntity<Otp> createAccount(@RequestBody Account account) {
-        Otp otp=Otp.builder().success(true).message("5412").build();
+        Otp otp=Otp.builder().success(true).message("541245").build();
         return ResponseEntity.ok(otp);
+    }
+
+    @PostMapping("/verifyOtp")
+    public ResponseEntity<otpResponse> verifyAccount(@RequestBody getOtp getOtp) {
+        boolean status = "123456".equals(getOtp.getOtp());
+
+        otpResponse otp = otpResponse.builder()
+                .success(status)
+                .message(status ? "Account verified successfully" : "Invalid OTP")
+                .build();
+
+        if (status) {
+            return ResponseEntity.ok(otp);
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(otp);
+        }
     }
 
 
